c79c5f40e2e44fc3d47431bfc5fdac2f616c5538
Adam Neujahr <adamneujahr@gmail.com>
2020-09-19 16:04:34 -0500

    Add routerlink for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
