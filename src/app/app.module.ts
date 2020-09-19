8c8904f566111a4ad9a7da5c9b1cf276b5adfb9d
Adam Neujahr <adamneujahr@gmail.com>
2020-09-19 16:20:28 -0500

    Add 404 Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     LoginComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
